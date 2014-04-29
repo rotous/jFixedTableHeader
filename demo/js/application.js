fa9d3ee30155f345ebc9c25a83d7808041863c7a
unknown <RT86425@.internal.epo.org>
2014-04-29 18:45:47 +0200

    Added new branch for using a seperate table for the header

--- demo/js/application.js
+++ demo/js/application.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
+/*
 	$("#fhTable").fixedTableHead({
 		'max-height': 400,
-		'max-width': '100%'
+		'max-width': '100%',
+		'width': '100%'
 	});
 	$("#fhTable2").fixedTableHead({
-		'height': 300
+		'height': 300,
+		'max-width': '100%',
+		'width': '50%'
 	});
+*/
+	$("#fhTable").fixedHeaderTable();
 });
--- jquery.fixedTableHead.js
+++ jquery.fixedTableHead.js
@@ -4,11 +4,18 @@
 	
 	var classPrefix = 'jfth-';
 	var tableClass = classPrefix + 'table';
-	var tableContainerClass = classPrefix + 'container';
+	var tableContainerClass = tableClass + 'container';
+	var tableCopyClass = tableClass+'-copy';
+	var tableCopyContainerClass = tableCopyClass+'-container';
 	var headClass = classPrefix + 'head';
 	
 	$.fn.fixedTableHead = function(cssProps) {
-
+		var $table;
+		var $tableCopy;
+		var $tableContainer;
+		var $tableCopyContainer;
+		var $cells;
+		
 		/**
 		 * Returns the z-index integer value for an element
 		 */
@@ -31,9 +38,67 @@
 		 * Wraps the table with a div and creates a fixed head row for the table
 		 */
 		function _createFixedHead(elTable){
+			$table = $(elTable);
 			
-			if ( !$(elTable).hasClass(tableClass) ){
+			if ( !$table.hasClass(tableClass) ){
+				
+				//Check if there is at least one row
+				if ( $("tr", $table).length === 0 ){ return; }
+				
+				//Copy the table
+				$tableCopy = $table.clone();
+				$tableCopy.removeAttr('id');
+				
+				//Remove all rows except the first
+				$("tr", $tableCopy).not(":eq(0)").remove();
+				
+				var props = ['border', 'background', 'color', 'font', 'padding', 'borderCollapse'];
+				
+				//Copy the visual properties of the table
+				for ( var i=0; i<props.length; i++ ){
+					$tableCopy.css(props[i], $table.css(props[i]));
+				}
+				$tableCopy.css('borderBottom', 'none !important');
 
+				//Copy the visual properties of the first row
+				var $tr = $("tr", $table).eq(0);
+				for ( var i=0; i<props.length; i++ ){
+					console.log('props['+i+']=', props[i], $("tr", $table).css(props[i]))
+					$("tr", $tableCopy).css(props[i], $tr.css(props[i]));
+				}
+
+				//Copy the visual properties of cells of the first row
+				$cells = $("> *", $tr);
+				$("tr > *", $tableCopy).each(function(index){
+					var $cell = $cells.eq(index);
+					for ( var i=0; i<props.length; i++ ){
+						$(this).css(props[i], $cell.css(props[i]));
+					}
+				});
+				
+				//TODO: check for thead or tbody and copy its visual properties
+				
+				//Wrap the copy with a div
+				$tableCopy.addClass(tableCopyClass).wrap('<div class="'+tableCopyContainerClass+'" />');
+				$tableCopyContainer = $tableCopy.parent("."+tableCopyContainerClass);
+				$tableCopyContainer.css({
+					'position': 'absolute',
+					'left': 0,
+					'top': 0,
+				});
+				
+				//Wrap the original table with a div
+				$table.addClass(tableClass).wrap('<div class="'+tableContainerClass+'" />');
+				$tableContainer = $table.parent("."+tableContainerClass);
+				cssProps.overflow = 'auto';
+				$tableContainer.css(cssProps);
+				if ( !('position' in cssProps) ){ $tableContainer.css('position', 'relative'); }
+				
+				//Append the copy to the wrapping div of the original
+				$tableCopyContainer.css('zIndex', _findZIndex(elTable));
+				$tableContainer.append($tableCopyContainer);
+
+/*
 				//Copy first row of the table
 				var $firstRow = $("tr", elTable).eq(0);
 				var $fixedRow = $firstRow.clone();
@@ -81,19 +146,32 @@
 			    	$firstDataRow.find("> *").css('borderTopWidth', 0);
 			    	
 		    	}
+*/	    	
 			}
-	    	
 		}
 		
 		function _resizeHeaderCells(elTable){
 	    	
+			//Resize the width of the table
+			console.log('tableWidth=', $table.width(), $table.css('width'))
+			$tableCopy.width($table.width());
+			$tableCopyContainer.width($table.width());
+			
 	    	//resize the width of the cells
+			$("tr > *", $tableCopy).each(function(index){
+				$(this).width( $cells.eq(index).width() );
+				$(this).css('padding', $cells.eq(index).css('padding'));
+				$(this).css('maxWidth', $cells.eq(index).css('maxWidth'));
+				$(this).css('minWidth', $cells.eq(index).css('minWidth'));
+			});
+			
+/*
 			var $tr = $("tr."+headClass, elTable);
 			var $tds = $("> *", $tr);
 	    	$("tr", elTable).eq(0).children().each(function(index){
-	    		console.log('setting '+index+' to '+$(this).width())
-	    		$tds.eq(index).width(($(this).width()+13)+'px');
-	    		console.log('width is now '+$tds.eq(index).width())
+//	    		console.log('setting '+index+' to '+$(this).width())
+	    		$tds.eq(index).width(($(this).width())+'px');
+//	    		console.log('width is now '+$tds.eq(index).width())
 	    	});
 	    	
 	    	//Set the width of the container
@@ -105,6 +183,7 @@
 	    	
 	    	//Set the width of the row
 	    	$tr.width(containerWidth-scrollbarWidth);
+*/
 		}
 		
 		function _resizeCells(elTable){
@@ -120,13 +199,17 @@
 				ready = true;
 				_resizeHeaderCells(elTable);
 				$firstDataRowCells.each(function(index){
-					console.log($(this).width(), $fixedHeaderRowCells.eq(index).width())
+//					console.log($(this).width(), $fixedHeaderRowCells.eq(index).width())
 					if ( $(this).width() !== $fixedHeaderRowCells.eq(index).width() ){ ready = false; }
 				});
 				
 				if ( ++counter >= 10 ) break;
 			}while( !ready );
-			console.log("counter=", counter)
+			
+			$("td,th", elTable).each(function(){
+				$(this).html($(this).width());
+			});
+//			console.log("counter=", counter)
 		}
 		
     	var oldScrollTop = $(this).parent("."+tableContainerClass).scrollTop();
@@ -136,17 +219,18 @@
 	    	_createFixedHead(this);
 	    	
 	    	$(window).resize(function(){
-
-				_resizeCells(self);
+	    		_resizeHeaderCells(self);
 	    		$(window).scroll();
 	    	});
-	    	_resizeCells(self);
 	    	
 	    	$(window).scroll(function(){
 	    		$("tr."+headClass, self).css('top', $(self).parent().offset().top-$(document).scrollTop());
 	    		$("tr."+headClass, self).css('left', $(self).parent().offset().left-$(document).scrollLeft());
 	    	});
+
+	    	_resizeHeaderCells(self);
 	    	$(window).scroll();
+	    	setTimeout(function(){_resizeHeaderCells(self);}, 2000);
 	    	
 	    	$(this).parent('.'+tableContainerClass).scroll(function(e){
 	    		var st = $(self).parent("."+tableContainerClass).scrollTop();
